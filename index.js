d561b4debd0754a5ab98b1d23060ace3049e0f7f
leulseged gebremedhin <leulsegedgebremedhin@gmail.com>
2023-07-28 15:05:05 +0300

    commited

---
+++ index.js
@@ -0,0 +1,10 @@
+// const express = require("express");
+// app = express();
+
+// app.listen(5000, () => console.log("listening on port 5000"));
+
+// app.get("/", (req, res) => {
+//   res.status(200).send("<h1>hello world<h1>");
+// });
+
+console.log("hello world");
