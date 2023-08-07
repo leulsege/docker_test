a5892dfeb0b7c8ca454d240cc943a36bc4293244
leulseged gebremedhin <leulsegedgebremedhin@gmail.com>
2023-08-07 03:45:43 +0300

    modify

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-// const express = require("express");
-// app = express();
+const express = require("express");
+app = express();
 
-// app.listen(5000, () => console.log("listening on port 5000"));
+app.listen(5000, () => console.log("listening on port 5000"));
 
-// app.get("/", (req, res) => {
-//   res.status(200).send("<h1>hello world<h1>");
-// });
-
-console.log("hello world from updated dockerhub");
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>hello world<h1>");
+});
