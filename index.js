3acb4e99800ba198f57e13d920ccaef3e0d2c9fb
leulseged gebremedhin <leulsegedgebremedhin@gmail.com>
2023-08-07 04:12:14 +0300

    h

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ app = express();
 app.listen(5000, () => console.log("listening on port 5000"));
 
 app.get("/", (req, res) => {
-  res.status(200).send("<h1>hello world<h1>");
+  res.status(200).send("<h1>hello o world<h1>");
 });
